f15bb05698f005825bbebac9fb689de87a883141
Oliver White <whiteoliverg@gmail.com>
2022-07-23 19:50:31 -0400

    begin working on simple file uploading

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback} from 'react';
+import {useDropzone} from 'react-dropzone';
+import styled from '@emotion/styled';
+
+
+const Dropzone = styled.div`
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+  width: 100vw;
+  height: 100vh;
+`
 
 function App() {
+  const onDrop = useCallback((acceptedFiles: any) => {
+    console.log(acceptedFiles);
+  }, [])
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Dropzone {...getRootProps()}>
+      <input {...getInputProps()} />
+      <header>
+        <h1>
+          Let's upload some images!!
+        </h1>
       </header>
-    </div>
+      <p>
+        Drag and drop or select a file.
+      </p>
+    </Dropzone>
   );
 }
 
